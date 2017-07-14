ecb6d21c0d77354667abc7c93cd47f105abb136a
micheldavalos <michel.davalos@academicos.udg.mx>
2017-07-14 00:21:41 -0500

    se recibe y se convierte a JSON

--- src/motores.cpp
+++ src/motores.cpp
@@ -1,6 +1,11 @@
 #include <Motores.h>
 #include <SoftwareSerial.h>
+#include <ArduinoJson.h>
 
+String json_data = "";
+      // "{\"sensor\":\"gps\",\"time\":1351824120,\"data\":[48.756080,2.302038]}";
+char character;
+StaticJsonBuffer<512> jsonBuffer;
 
 void setup(void)
 {
@@ -9,18 +14,32 @@ void setup(void)
   Motores m;
   uint8_t status = m.agregarMotor(5);
 
-  if (status == 1)
-  {
-    m.mover(5, 512);
-    delay(500);
-    m.mover(5, 666);
-
-  }
-
+  // if (status == 1)
+  // {
+  //   m.mover(5, 512);
+  //   delay(500);
+  //   m.mover(5, 666);
+  //
+  // }
 
 
 }
 
 void loop(void)
 {
+  if (Serial.available())
+  {
+    while(Serial.available()) {
+          character = Serial.read();
+          json_data.concat(character);
+          // Serial.println(json_data);
+          if (character == '}')
+          {
+            JsonObject& root = jsonBuffer.parse(json_data);
+            root.printTo(Serial);
+            json_data = "";
+          }
+      }
+  }
+
 }
